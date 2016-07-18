41e3da20e0cb5ae4c48e35f4660bb894618354ce
Zhang <Bert.Zhang@YRCFreight.com>
2016-07-18 18:09:46 -0500

    2st

--- Sysnet/src/Test/T1.java
+++ Sysnet/src/Test/T1.java
@@ -19,54 +19,55 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import Utility.ConfigRd;
-import page.CommonData;
 import page.SysnetPage;
 
 public class T1 {
-	
-	
-	 private WebDriver driver;
-	 private SysnetPage page;
-	 private Actions builder;
 
- @BeforeClass
- @Parameters({"browser"})
- public void SetUp(@Optional("chrome")String browser) throws AWTException, InterruptedException, IOException { 	  
-	  ConfigRd Conf= new ConfigRd();	  
-	  if (browser.equalsIgnoreCase("chrome")){
- 	  System.setProperty("webdriver.chrome.driver", Conf.GetChromePath());
- 	  driver = new ChromeDriver();            
- 	  }else if(browser.equalsIgnoreCase("ie")){
- 	  System.setProperty("webdriver.ie.driver", Conf.GetIEPath());
- 	  driver=new InternetExplorerDriver();
- 	  }
-   page=new SysnetPage(driver);
-   driver.get(Conf.GetSysnetSitURL());
-   driver.manage().window().maximize();
-   builder= new Actions(driver);
-	  }
+	private WebDriver driver;
+	private SysnetPage page;
+	private Actions builder;
 
-  
- @Test
-  public void GetTrailerInformation() {
-  (new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.SystemSummaryButton));
-  page.SystemSummaryButton.click();
-  String CurrentWindowHandle=driver.getWindowHandle();
-  (new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.InterRegionalform));
-  page.InterRegionalform.findElement(By.xpath("//span[contains(text(), 'Average Time for LDD + ARV in Hours')]")).click();
-  (new WebDriverWait(driver, 50)).until(ExpectedConditions.numberOfWindowsToBe(2));
-  Set<String> WindowHandles=driver.getWindowHandles();
-  for(String windowHandle:WindowHandles ){
-   if(!windowHandle.equalsIgnoreCase(CurrentWindowHandle)) {driver.switchTo().window(windowHandle); }}
-  (new WebDriverWait(driver, 20)).until(ExpectedConditions.invisibilityOfElementLocated(By.id("loading-bar")));
-  (new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.ATLddArvTrailerInforGrid));
-  LinkedHashSet<ArrayList<String>> TrailerGRID=page.GetTrailerInfoList(page.ATLddArvTrailerInforGrid);
-  System.out.println(TrailerGRID.size());
-  for(ArrayList<String> line:TrailerGRID){
-	  System.out.print("\n"); 
-	  for(String information:line){
-	  System.out.print(information+" ");  
-	  }
-  }
-  } 
+	@BeforeClass
+	@Parameters({ "browser" })
+	public void SetUp(@Optional("chrome") String browser) throws AWTException, InterruptedException, IOException {
+		ConfigRd Conf = new ConfigRd();
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", Conf.GetChromePath());
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", Conf.GetIEPath());
+			driver = new InternetExplorerDriver();
+		}
+		page = new SysnetPage(driver);
+		driver.get(Conf.GetSysnetSitURL());
+		driver.manage().window().maximize();
+		builder = new Actions(driver);
+	}
+
+	@Test
+	public void GetTrailerInformation() {
+		(new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.SystemSummaryButton));
+		page.SystemSummaryButton.click();
+		String CurrentWindowHandle = driver.getWindowHandle();
+		(new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.InterRegionalform));
+		page.InterRegionalform.findElement(By.xpath("//span[contains(text(), 'Average Time for LDD + ARV in Hours')]"))
+				.click();
+		(new WebDriverWait(driver, 50)).until(ExpectedConditions.numberOfWindowsToBe(2));
+		Set<String> WindowHandles = driver.getWindowHandles();
+		for (String windowHandle : WindowHandles) {
+			if (!windowHandle.equalsIgnoreCase(CurrentWindowHandle)) {
+				driver.switchTo().window(windowHandle);
+			}
+		}
+		(new WebDriverWait(driver, 20)).until(ExpectedConditions.invisibilityOfElementLocated(By.id("loading-bar")));
+		(new WebDriverWait(driver, 50)).until(ExpectedConditions.visibilityOf(page.ATLddArvTrailerInforGrid));
+		LinkedHashSet<ArrayList<String>> TrailerGRID = page.GetTrailerInfoList(page.ATLddArvTrailerInforGrid);
+		System.out.println(TrailerGRID.size());
+		for (ArrayList<String> line : TrailerGRID) {
+			System.out.print("\n");
+			for (String information : line) {
+				System.out.print(information + " 7777");
+			}
+		}
+	}
 }
